44052cea806bd2d2ef85dc2d79806076e4d93487
palmcivet7 <palmcivet@proton.me>
2023-08-23 19:33:20 +0100

    For Loops

---
+++ Fibonacci/src/Fibonacci.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract Fibonacci {
+    /*
+        This exercise assumes you understand what Fibonacci sequence is.
+        1. Function `fibonacci` takes a uint256 as argument and returns nth fibonacci number.
+        
+        Fibonacci sequence are 0,1,1,2,3,5,8,13,....
+        
+        calling fibonacci(6) would return 8, because the 6th Fibonacci number is 8.
+    */
+
+    function fibonacci(uint256 _position) public view returns (uint256) {
+        // your code here
+        if (_position == 0) {
+            return 0;
+        } else if (_position == 1) {
+            return 1;
+        } else {
+            uint256 a = 0;
+            uint256 b = 1;
+            for (uint256 i = 2; i <= _position; i++) {
+                uint256 temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return b;
+        }
+    }
+}
---
+++ IsPrime/src/IsPrime.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract IsPrime {
+    /**
+     * The goal of this exercise is to return if "number" is prime or not (true or false)
+     */
+    function isPrime(uint256 number) public pure returns (bool) {
+        for (uint256 i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
