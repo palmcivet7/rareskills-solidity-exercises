c07a1ffbf57f9fd64b733d218f1811ade5c61158
palmcivet7 <palmcivet@proton.me>
2023-08-26 18:07:12 +0100

    Receive

---
+++ BasicBankV2/src/BasicBankV2.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract BasicBankV2 {
+    error InsufficientBalance();
+    /// used to store the balance of users
+    ///     USER    =>  BALANCE
+
+    mapping(address => uint256) public balances;
+
+    /// @notice deposit ether into the contract
+    /// @dev it should work properly when called multiple times
+    function addEther() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    /// @notice used to withdraw ether from the contract
+    /// @param amount of ether to remove. Cannot execeed balance i.e users cannot withdraw more than they deposited
+    function removeEther(uint256 amount) external payable {
+        if (amount > balances[msg.sender]) {
+            revert InsufficientBalance();
+        }
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
---
+++ DistributeV2/src/DistributeV2.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract DistributeV2 {
+    /*
+        This exercise assumes you know how to sending Ether.
+        1. This contract has some ether in it, distribute it equally among the
+           array of addresses that is passed as argument.
+        2. Write your code in the `distributeEther` function.
+        3. Consider scenarios where one of the recipients rejects the ether transfer, 
+           have a work around for that whereby other recipients still get their transfer
+    */
+
+    event TransferFailed(address recipient);
+
+    constructor() payable {}
+
+    function distributeEther(address[] memory addresses) public {
+        // your code here
+        uint256 balance = address(this).balance;
+        uint256 distributedAmount = balance / addresses.length;
+        for (uint256 i = 0; i < addresses.length; i++) {
+            (bool success,) = payable(addresses[i]).call{value: distributedAmount}("");
+            if (!success) {
+                emit TransferFailed(addresses[i]);
+            }
+        }
+    }
+}
